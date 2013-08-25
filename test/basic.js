78fef697854c99479084988cb19088da118e3a2d
Dominic Tarr <dominic.tarr@gmail.com>
2013-08-25 21:44:58 +0100

    at least test it doesn't throw

--- test/basic.js
+++ test/basic.js
@@ -2,7 +2,19 @@
 var Rec2 = require('../')
 var Vec2 = require('vec2')
 
-var r = new Rec2(1, 0, 10, 10)
+var rec = new Rec2(1, 0, 10, 10)
 
 console.log(Object.hasOwnProperty(r, 'x'), this.x, this.y)
-console.log(r)
+console.log(rec)
+
+function r () {
+  return Math.random() * 1000
+}
+
+var l = 1000
+while (l--) {
+  var rec = new Rec2(r(), r(), r(), r())
+  rec.size.set(r(), r())
+  rec.set(r(), r())
+  console.log(rec)
+}
